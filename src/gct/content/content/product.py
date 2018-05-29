8b000f175cd232694ee628d99d1f7109b0704a47
Kiel <kiel@mingtak.com.tw>
2018-05-29 06:07:12 +0000

    update content_type: Product

--- src/gct/content/content/product.py
+++ src/gct/content/content/product.py
@@ -1,15 +1,40 @@
 # -*- coding: utf-8 -*-
+from gct.content import _
 from plone.dexterity.content import Container
 from plone.supermodel import model
 from zope.interface import implementer
-
+from zope import schema
+from plone.app.textfield import RichText
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile, NamedImage
 
 class IProduct(model.Schema):
-    """ Marker interface for Product
-    """
+    name = schema.TextLine(
+        title=_(u'Name'),
+        required=True,
+    )
 
+    modelNo = schema.TextLine(
+        title=_(u'Model No'),
+        required=False,
+    )
 
-@implementer(IProduct)
-class Product(Container):
-    """
-    """
+    beApplicable = schema.TextLine(
+        title=_(u'Be Applicable'),
+        required=False,
+    )
+
+    characteristic = schema.TextLine(
+        title=_(u'Characteristic'),
+        required=False,
+    )
+
+    description = RichText(
+        title=_(u'Description'),
+        required=False,
+    )
+
+    cover = NamedBlobImage(
+        title=_(u"Cover Image"),
+        required=False,
+    )
+
--- src/gct/content/interfaces.py
+++ src/gct/content/interfaces.py
@@ -2,7 +2,11 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from plone.dexterity.content    import Container
 
+
+class FolderishContent(Container):
+    pass
 
 class IGctContentLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
